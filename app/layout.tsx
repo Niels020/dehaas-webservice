06c522bd757409fb30d271d4fdf9947527c162b2
Niels de Haas <nielsdehaas020@gmail.com>
2026-05-16 15:13:10 +0200

    Re-enable under construction overlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
 				/>
 			</head>
 			<body className="flex min-h-full flex-col">
-				{/* <UnderConstruction /> */}
+				<UnderConstruction />
 				<Header />
 				<main id="main-content" className="flex flex-1 flex-col">
 					{children}
